88f40d9e926a35e0e0c6c7da1d523f01cd0e0a73
DevAlePeppermint <xandagropp@hotmail.com>
2021-04-18 20:05:25 -0300

    type null

---
+++ output/null.js
@@ -0,0 +1,9 @@
+"use strict";
+let value;
+console.log(typeof value);
+let despesaFixa;
+let despesaVariavel;
+despesaFixa = "150";
+despesaVariavel = 320;
+console.log(despesaFixa + despesaVariavel);
+//# sourceMappingURL=null.js.map
---
+++ scripts/null.ts
@@ -0,0 +1,12 @@
+let value: any;
+
+console.log(typeof value);
+
+
+let despesaFixa: any;
+let despesaVariavel: number;
+
+despesaFixa = "150";
+despesaVariavel = 320;
+
+console.log(despesaFixa + despesaVariavel);
