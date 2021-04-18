38a86fbaf29955dc225a9b335ca085a51a71d2ce
DevAlePeppermint <xandagropp@hotmail.com>
2021-04-18 19:28:30 -0300

    Tipo null

--- output/null_undefined.js
+++ output/null_undefined.js
@@ -1,2 +1,8 @@
 "use strict";
+let valor;
+valor = null;
+const elementoHTML = document.querySelector("h2");
+let minhaVariavel; //aqui ela está undefined, mas ainda dinamico.
+let minhaVariavelAmarrada = undefined; // aqui nao pode mais receber nenhum valor
+/* SALVO EXCEÇÕES, AS VARIAVEIS QUE POSSUEM TIPO NULL E UNDEFINED VEM COM PIPE -> | <- */ 
 //# sourceMappingURL=null_undefined.js.map
---
+++ scripts/null_undefined.ts
@@ -0,0 +1,11 @@
+let valor:null;
+valor = null; 
+
+const elementoHTML : HTMLHeadingElement | null = document.querySelector("h2");
+
+
+
+let minhaVariavel //aqui ela está undefined, mas ainda dinamico.
+let minhaVariavelAmarrada:undefined = undefined // aqui nao pode mais receber nenhum valor
+
+/* SALVO EXCEÇÕES, AS VARIAVEIS QUE POSSUEM TIPO NULL E UNDEFINED VEM COM PIPE -> | <- */
